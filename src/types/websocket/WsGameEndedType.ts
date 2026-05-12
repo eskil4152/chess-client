a446d069107e01063750fb4135ce8d79adc834aa
eskil <eskil@blikeng.com>
2026-05-12 17:22:35 +0200

    Draw types
    - Draw types implemented
    - Replace underscore with spaces in draw types

--- src/types/websocket/WsGameEndedType.ts
+++ src/types/websocket/WsGameEndedType.ts
@@ -1,6 +1,6 @@
 import { GameStatus } from "../http/GameStatus";
 
-export type EndedBy = "checkmate" | "agreement" | "resignation" | "stalemate";
+export type EndedBy = "checkmate" | "agreement" | "resignation" | "stalemate" | "repetition" | "insufficient_material" | "fifty_move_rule";
 
 export type WsGameEndedType = {
   type: "GAME_ENDED";
